1e4a3bfeb2e1f44e5eecfa70c01853389e200349
Rigo Rosero Castillo <rigo4791@gmail.com>
2025-08-14 21:13:13 +0900

    adds the adapters and echo server

--- cmd/server.go
+++ cmd/server.go
@@ -1,3 +1,21 @@
 package cmd
 
-// Factory function - no depende de este dominio
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func StartServer() error {
+	e := echo.New()
+	// middleware basico de echo
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello world")
+	})
+	e.Logger.Info("Server starting on :8080")
+	return e.Start(":8080")
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"log"
+	"restaurant-go-api/cmd"
+)
+
+func main() {
+	if err := cmd.StartServer(); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+}
